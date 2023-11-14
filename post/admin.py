577fff89355c45ff004162c5b1bf126039290e88
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-14 09:41:33 +0330

    create a panel admin and make filter on it

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "status", "author", "date_and_time_modified"]
+    ordering = ("-status", )
+
+
+admin.site.register(Post, PostAdmin)
+
