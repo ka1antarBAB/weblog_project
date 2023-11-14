1c72d38d98389a7681b363cdc6a3ef3008a5a753
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-14 09:40:32 +0330

    make models for post (text,title,author,status)

--- post/models.py
+++ post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib import auth
+
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ("pub", "Published"),
+        ("drf", "Draft")
+    )
+    text = models.CharField(max_length=100)
+    title = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+    date_and_time_created = models.DateTimeField(auto_now_add=True)
+    date_and_time_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+
