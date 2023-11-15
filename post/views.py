c67241f9d03b030e3c8152e202e31f3493b6e633
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-15 11:55:22 +0330

    just a random change

--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from . models import Post
 # Create your views here.
+
+
+def post_list_view(request):
+    post = Post.objects.all()
+    context = {
+        "post": post
+    }
+    return render(request, "post/list_view.html", context=post)
