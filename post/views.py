0f81b8ce2fa0314dab0924a74f43de3510b3c454
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-15 12:36:59 +0330

    make a simple view for test the Post-modle

--- post/views.py
+++ post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "post/posts_list.html", {"posts_list": posts})
