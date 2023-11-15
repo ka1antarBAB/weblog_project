b6e599599c305e64bd6b4f588a6771002e1b9c13
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-15 12:13:01 +0330

    change admin registaer style with @

--- post/admin.py
+++ post/admin.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 
-# Register your models here.
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "status", "author", "date_and_time_modified"]
     ordering = ("-status", )
 
-
-admin.site.register(Post, PostAdmin)
 
