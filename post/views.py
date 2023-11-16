177615ecca3293b8e4cfec5c464cbe16aa98124b
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-16 15:55:30 +0330

    add get_object_or_404 for post detail view

--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -9,5 +10,10 @@ def post_list_view(request):
 
 
 def post_detail_view(request, pk):
-    posts = Post.objects.get(pk=pk)
+    posts = get_object_or_404(Post, pk=pk)
+    # try:
+    #     posts = Post.objects.get(pk=pk)
+    # except Post.DoesNotExist as error:
+    #     posts = None
+    #     print(error)
     return render(request, "post/post_detail.html", {"post_detail": posts})
