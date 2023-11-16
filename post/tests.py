5085fe77d861a0cf7ae74c4c8c96fdd1844b5d75
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-16 16:31:24 +0330

    add test for blog  by name and url

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+
 
 # Create your tests here.
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="userCheck"
+        )
+        self.post = Post.objects.create(
+            title="title check",
+            text="text check",
+            status=Post.STATUS_CHOICES[0][0],
+            author=self.user,
+        )
+
+    def test_post_list_url(self):
+        response = self.client.get(reverse("post_list_view"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_post_list_by_name(self):
+        response = self.client.get("/blog/")
+        self.assertEquals(response.status_code, 200)
+
