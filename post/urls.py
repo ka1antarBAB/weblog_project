25e969799ea825b3649fed63989915916b143e49
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-19 09:12:07 +0330

    rename template file (post_add) -> (post_create)

--- post/urls.py
+++ post/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 
 urlpatterns = [
-    path("", views.post_list_view, name="post_list_view"),
-    path("<int:pk>/", views.post_detail_view, name="post_detail_view")
+    path("", views.post_list_view, name=""),
+    path("<int:pk>/", views.post_detail_view, name="post_detail_view"),
+    path("add/", views.post_add_view, name="post_add_view")
 ]
