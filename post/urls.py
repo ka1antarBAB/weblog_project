4b5cab0e2b005e42a076b345557bd0286dbd5031
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-19 15:44:13 +0330

    make url for update view

--- post/urls.py
+++ post/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.post_list_view, name="post_list_view"),
     path("<int:pk>/", views.post_detail_view, name="post_detail_view"),
     path("add/", views.post_add_view, name="post_create_view"),
-
+    path("<int:pk>/update/", views.post_update_view, name="post_update_view")
 ]
