e2011a899d607df4e675a914df91cd470c471725
kalantarBAB <115296752+kalantarBAB@users.noreply.github.com>
2023-11-21 09:40:25 +0330

    turn all of function views to the class view in urls.py (.as_view())

--- post/urls.py
+++ post/urls.py
@@ -3,10 +3,10 @@ from .import views
 
 
 urlpatterns = [
-    path("", views.post_list_view, name="post_list_view"),
-    path("<int:pk>/", views.post_detail_view, name="post_detail_view"),
-    path("add/", views.post_add_view, name="post_create_view"),
-    path("<int:pk>/update/", views.post_update_view, name="post_update_view"),
-    path("<int:pk>/delete/", views.post_delete_view, name="post_delete_view"),
+    path("", views.PostListView.as_view(), name="post_list_view"),
+    path("<int:pk>/", views.PostDetailView.as_view(), name="post_detail_view"),
+    path("add/", views.PostCreateView.as_view(), name="post_create_view"),
+    path("<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update_view"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete_view"),
 
 ]
